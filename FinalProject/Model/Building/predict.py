724101b3326663ad24efa195291706bbd843e811
SiyangJ <siyangj@live.unc.edu>
2018-11-18 16:06:42 -0500

    First version of test/inference functionality. For test/experiment/demonstration purpose only.

---
+++ FinalProject/Model/Building/predict.py
@@ -0,0 +1,47 @@
+from config import FLAGS
+import os.path
+import numpy as np
+import math
+import h5py
+import time
+import tensorflow as tf
+from generator import get_training_and_testing_generators
+
+## A very crude version.
+## Just for experiment.
+def predict(td):
+    preds = []
+    X_test = []
+    Y_test = []
+    start_time  = time.time()
+    _, test_generator = get_training_and_testing_generators()
+    ## TODO
+    ## Just for the purpose of testing.
+    patch_num = 20
+    print '>> begin predicting for each patch'
+    for _i in  range(patch_num):
+        _test_X, _test_Y = next(test_generator)
+        feed_dict = { td.X_variable : _test_X, 
+                     td.Y_variable : _test_Y}
+        ops = [td.pred,]
+        [pred,] = td.sess.run(ops, feed_dict=feed_dict)
+        preds.append(pred)
+        X_test.append(_test_X)
+        Y_test.append(_test_Y)
+        
+    array_pred = np.asarray(preds)
+    #print '>> begin vote in overlapped patch..'
+    #seg_res, possibilty_map = vote_overlapped_patch(patches_pred, index, d,h,w)
+    # seconds
+    elapsed = int(time.time() - start_time)
+
+    print('Predict complete, cost [%3d] seconds' % (elapsed))
+
+    return X_test, Y_test, array_pred
+
+def main():
+    predict(None)
+
+if __name__ == '__main__':
+    main()
+    
---
+++ FinalProject/Model/Building/test.py
@@ -0,0 +1,77 @@
+import tensorflow as tf
+from model import create_model, create_optimizers
+from train import train_model
+import random
+import numpy as np
+import os
+# from generator import get_training_and_testing_generators
+from predict import predict
+import generator
+from copy import deepcopy
+from config import FLAGS
+
+
+
+def prepare_dirs(delete_train_dir=False):
+	# Create checkpoint dir (do not delete anything)
+	if not tf.gfile.Exists(FLAGS.checkpoint_dir):
+		tf.gfile.MakeDirs(FLAGS.checkpoint_dir)
+	
+	# Cleanup train dir
+	if delete_train_dir:
+		if tf.gfile.Exists(FLAGS.train_dir):
+			tf.gfile.DeleteRecursively(FLAGS.train_dir)
+		tf.gfile.MakeDirs(FLAGS.train_dir)
+
+def setup_tensorflow():
+	
+	config = tf.ConfigProto()
+	sess = tf.Session(config=config)
+
+	# Initialize rng with a deterministic seed
+	with sess.graph.as_default():
+		tf.set_random_seed(FLAGS.random_seed)
+		
+	random.seed(FLAGS.random_seed)
+	np.random.seed(FLAGS.random_seed)
+
+	tf.gfile.MkDir('%s/test_log' % (FLAGS.save_path,))
+	summary_writer = tf.summary.FileWriter('%s/test_log' % (FLAGS.save_path,), sess.graph)
+	return sess, summary_writer
+
+
+
+class TestData(object):
+	def __init__(self, dictionary):
+		self.__dict__.update(dictionary)
+
+
+
+
+def test():
+	prepare_dirs(delete_train_dir=False)
+	sess, summary_writer = setup_tensorflow()
+
+	(X_variable, Y_variable,
+     pred, loss, final_loss,
+     gene_vars) = create_model(train_phase=True)
+
+	saver = tf.train.Saver()
+	## Load from the model save path instead of the last_trained_checkpoint, 
+	## which is supposed to be the save_path of last training
+	model_path = tf.train.latest_checkpoint(FLAGS.save_path)
+	print('saver restore from:%s' % model_path)
+	saver.restore(sess, model_path)
+	
+	test_data = TestData(locals())
+	X_test, Y_test, array_pred = predict(test_data)
+	return X_test, Y_test, array_pred
+
+
+def main(argv=None):
+	print ('>> start testing phase...')
+	test()
+
+
+if __name__ == '__main__':
+	tf.app.run()
